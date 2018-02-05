32102e90ae0190c59ff6903efc19ee20d2679057
unknown <mikailbosna98@gmail.com>
2018-02-05 15:41:51 +0100

    added random quote to home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import Joke from '../Joke/Joke';
 import "./Home.css"
+import CircularProgress from 'material-ui/CircularProgress';
+
 
 class Home extends Component {
     constructor(props){
         super(props);
         this.state={
-            joke:{}
+            joke:{},
+            loading:true
         }
     }
   componentDidMount(){
       fetch("https://api.chucknorris.io/jokes/random")
       .then(res=>res.json())
-      .then(res=>this.setState({joke:res}))
+      .then(res=>this.setState({joke:res,loading:false}))
   }
   render() {
     return (
       <div className="home">
-        
+      {this.state.loading?
+        <CircularProgress className="loader" size={200} thickness={5} />:
+        <div>
+          <img src="https://assets.chucknorris.host/img/chucknorris_logo_coloured_small@2x.png" alt="Chuck Norris" className="chuck-image"/>
+          <Joke joke={this.state.joke} icon="https://assets.chucknorris.host/img/avatar/chuck-norris.png"/>
+        </div>}
       </div>
     );
   }
